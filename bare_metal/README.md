226c7ffebd17bbdc239ce7cde677b3f105aaad61
Chris <chrismmalecki@gmail.com>
2026-01-20 15:33:52 -0500

    bare metal STM32 project with F411CEU6 for DIY Power Supply

---
+++ bare_metal/README.md
@@ -0,0 +1,9 @@
+# Bare Metal STM32 project - DIY Bench Power Supply
+
+## Prerequisites
+gcc, arm-none-eabi-gcc, gcc-multiarch, bear, clangd
+
+##
+Make and Makefile
+Linker script
+Drivers
---
+++ bare_metal/src/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void SystemInit(void)
+{
+    // Empty for now - clock config goes here later
+}
+
+int main(void)
+{
+    uint32_t x = 0;
+    while (1)
+        ;
+}
